df285646afa3606eea29718776c508bbe1330a82
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2026-08-13 15:40:53 -0400

    new parameter.h adapted to the new par_get.f90

--- src/parameter.h
+++ src/parameter.h
@@ -1,34 +1,72 @@
 !------------------------------------------------------------------------
 ! Defines the size and position of the domain and num parameters
 !
-! Multi resolution 2008 version 
+! Multi resolution 2026 version 
+! (backwards compatibility is ensured, old grids still work)
+! jkr: Definition of dx_pole and dy_pole is now done dynamically
+! in par_get.f90
 !
-! Resolution [km]      nx        ny
+! base 32 grids
+!
+! Resolution dx [km]   nx        ny
 ! ---------------------------------
-!         10          518       438
-!         20          258       218
-!         40          128       108
-!         80           63        53
+!        1.00         5120      4320
+!        2.00         2560      2160
+!        4.00         1280      1080
+!        8.00          640       540
+!       16.00          320       270
+!       32.00          160       135
 ! ---------------------------------
+!
+! base 80 grids
+! old grids kept for backwards compatibility
+!
+! Resolution dx [km]   nx        ny
+! ---------------------------------
+!        1.25         4096      3456
+!        2.50         2048      1728
+!        5.00         1024       864
+!       10.00          512       432
+!       20.00          256       216
+!       40.00          128       108
+!       80.00           64        54
+! ---------------------------------
+!
+! Uniaxial loading experiments
+! uniaxial=.true.
+!
+! Resolution dx [km]   nx        ny
+! ---------------------------------
+!          1          100       250
+! ---------------------------------
+!
+! Ideal ice bridge experiments
+! ideal_bridge=.true.
+!
+! Resolution dx [km]   nx        ny
+! ---------------------------------
+!          2          102       402
+! ---------------------------------
+!
+!
 !------------------------------------------------------------------------
 
       integer nx, ny, ntot, nbuoy, nvar, img, img1
+      logical uniaxial, ideal_bridge
+      double precision Deltax, beta, S0
 
-      double precision beta, dx_pole, dy_pole, S0
-
-      parameter (                             &
-                nx    = 128,                   & ! x-dim of the domain
-                ny    = 108,                   & ! y-dim of the domain
-                dx_pole= 2500d3,              & ! tracer point (0,0) x distance from North pole [m] 
-                dy_pole= 2250d3,              & ! tracer point (0,0) y distance from North pole [m] 
-                beta  = 32.0d0,               & ! angle of the dom wr to Greenwich
-                ntot  = nx * ny,              & ! total number of cells
-                nvar  = (nx+1)*ny+(ny+1)*nx,  & ! total nb of u,v  var
-                img   = 50,                   & ! rstr value for FGMRES
-                img1  = img + 1,              & !
-                nbuoy = 3000,                 & ! number of buoys
-                S0 = 1340d0                   & ! Solar constant [W / m2]
+      parameter (                            &
+                nx = 160,                    & ! x-dim of the domain
+                ny = 135,                    & ! y-dim of the domain
+                Deltax = 32d03,              & ! grid resolution
+                ntot  = nx * ny,             & ! total number of cells
+                nvar  = (nx+1)*ny+(ny+1)*nx, & ! total nb of u,v  var
+                uniaxial = .false.,          & ! uniaxial experiment
+                ideal_bridge = .false.,      & ! ideal ice bridge experiment
+                beta = 32.0d0,               & ! angle of the dom wr to Greenwich
+                img = 50,                    & ! rstr value for FGMRES
+                img1 = img + 1,              & !
+                nbuoy = 3000,                & ! number of buoys
+                S0 = 1340d0                  & ! Solar constant [W / m2]
                 )
-
-
-
+      
